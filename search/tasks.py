47d12b3ba73eec1678b301e03cace82d542fd506
Bogdan Marchenko <bogdan.marchenko@zojax.com>
2013-02-14 16:36:44 +0200

    dfg

--- search/tasks.py
+++ search/tasks.py
@@ -25,24 +25,42 @@ def get_trains(data, attempt, res_base):
     base_dict = json.loads(res_base)
     base_trains = base_dict['trains']
     kype = data['kype']
-    platzkart = data['platzcart']
+    platzcart = data['platzcart']
+    new_kype = None
+    new_platzcart = None
 
     print 'yes news'
     msg = u"New tickets: "
     res_dict = json.loads(res)
     if res_dict.get('trains'):
+        print res
+        print 'comparing to'
+        print res_base
         for train in res_dict['trains']:
             base_train = [_ for i,_ in enumerate(base_trains) if _['train'] == train['train']]
             if base_train:
                 base_train = base_train[0]
                 if kype:
                     if base_train['k'] < train['k']:
-                        msg += 'Train %s new kype %d instead of %d /n' %(str(train['train']['0']), train['k'], base_train['k'])
-                if platzkart:
+                        msg += 'kype'
+                        msg += 'Train %s new kype %d instead of %d ' %(str(train['train']), train['k'], base_train['k'])
+                        new_kype = True
+                if platzcart:
+                    msg += 'pl'
                     if base_train['p'] < train['p']:
-                        msg += 'Train %s new platzcart %d instead of %d /n' %(str(train['train']['0']), train['p'], base_train['p'])
+                        msg += 'Train %s new platzcart %d instead of %d ' %(str(train['train']), train['p'], base_train['p'])
+                        new_platzcart = True
+                if kype and platzcart:
+                    if not new_kype or not new_platzcart:
+                        return get_trains.retry(args=[data, attempt, res])
+                if kype and not platzcart:
+                    if not new_kype:
+                        return get_trains.retry(args=[data, attempt, res])
+                if platzcart and not kype:
+                    if not new_platzcart:
+                        return get_trains.retry(args=[data, attempt, res])
             else:
-                msg+="new train!! %s with %d kype and %d platzcart" %(train['train'], train['k'], train['p'])
+                msg+="new train!! %s with %d kype and %d platzcart" %(str(train['train']), train['k'], train['p'])
             print msg
         send_mail("New tickets!", msg, 'bogdan.marko@gmail.com',
                       [data['email'],], fail_silently=False)
