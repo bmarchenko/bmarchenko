d5d683572347dd6a7b4fd91921766cbd098ec585
bmarchenko <bogdan.marko@gmail.com>
2013-06-04 12:57:54 +0300

    added social sharing

--- local_settings_template.py
+++ local_settings_template.py
@@ -18,8 +18,8 @@ DATABASES = {
     }
 }
 
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'youremail@gmail.com'
-EMAIL_HOST_PASSWORD = 'yourpassword'
-EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'youremail@gmail.com'
+# EMAIL_HOST_PASSWORD = 'yourpassword'
+# EMAIL_PORT = 587
